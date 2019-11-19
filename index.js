8982166c0639eafddbf72a20fda3f5648cfbddc9
Todd Carlson <todd.carlson3030@me.com>
2019-11-19 23:40:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   } else if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return "I love you, too."'
   }
 }
