1358abfc30792992c95509cae9fcf1fa86957904
Todd Carlson <todd.carlson3030@me.com>
2019-11-19 23:31:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string) {
-  return string.toUpperCase() 
+  return string.toUpperCase()
 }
 
 function whisper(string) {
-  return string.toLowerCase() 
+  return string.toLowerCase()
 }
 
 function logShout(string) {
@@ -13,13 +13,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can\'t hear you!";
-  } else if (string.toUpperCase() === string) {
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
-  }
+  if (string.toLowerCase()) {
+    return "I can't hear you!"
+  } else 
 }
