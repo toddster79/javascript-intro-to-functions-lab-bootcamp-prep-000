49dffd7b0dd483eb73f7f399c93bb0cee9bcef75
Todd Carlson <todd.carlson3030@me.com>
2019-11-19 23:40:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return "I can't hear you!";
